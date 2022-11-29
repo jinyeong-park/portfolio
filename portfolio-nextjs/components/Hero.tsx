c224c87a7ae916f8fd5557738fe37f43d2c70bc6
Jinyeong <jinyeongpark1205@gamil.com>
2022-11-29 02:59:12 -0800

    hero animation and styling

--- portfolio-nextjs/components/Hero.tsx
+++ portfolio-nextjs/components/Hero.tsx
@@ -6,17 +6,20 @@ type Props = {}
 
 export default function Hero({}: Props) {
     const [text] = useTypewriter({
-        words: ['Hello!, My name is Jenny.', '<I Love Building Things />'],
+        words: ["Hello!, I'm Jenny", '<I Love Building Things />'],
         loop: true,
-        delaySpeed: 2000,
+        delaySpeed: 2500,
       })
     
   return (
-    <div className="h-screen flex flex-col space-y-8 item-center justify-center">
+    <div className="h-screen flex flex-col space-y-8 items-center justify-center text-center overflow-hidden">
         <BackgroundCircles />
-        <h1>
-            <span>{text}</span>
-            <Cursor cursorColor='#F7AB0A' />
+   
+        <img className="relative rounded-full h-32 w-32 mx-auto object-cover" src="https://i.ibb.co/N7qq026/jenny0.jpg" />
+        <h2 className="text-sm uppercase text-gray-500 pb-2 tracking-[15px]">Software Engineer</h2>
+        <h1 className="text-5xl lg:tesxt-6xl font-semibold scroll-px-10">
+            <span className="mr-3">{text}</span>
+            <Cursor cursorColor="#F7AB0A" />
         </h1>
 
     </div>
