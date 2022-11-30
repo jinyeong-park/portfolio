934f6e1161673fca1e0db384e959977761673325
Jinyeong <jinyeongpark1205@gamil.com>
2022-11-30 10:31:27 -0800

    modify styling color

--- portfolio-nextjs/components/BackgroundCircles.tsx
+++ portfolio-nextjs/components/BackgroundCircles.tsx
@@ -22,7 +22,7 @@ export default function BackgroundCircles({}: Props) {
         <div className="border border-[#333333] rounded-full h-[300px] w-[300px] mt-52 absolute"/>
         <div className="border border-[#333333] rounded-full h-[500px] w-[500px] mt-52 absolute"/>
         <div className="border border-[#333333] rounded-full h-[500px] w-[500px] mt-52 absolute"/>
-        <div className="border border-[#F7AB0A] rounded-full h-[650px] w-[650px] mt-52 absolute animate-pulse"/>
+        <div className="border border-[#aeaea0] rounded-full opacity-20 h-[650px] w-[650px] mt-52 absolute animate-pulse"/>
         <div className="border border-[#333333] rounded-full h-[800px] w-[800px] mt-52 absolute"/>
     </motion.div>
   );
--- portfolio-nextjs/components/Hero.tsx
+++ portfolio-nextjs/components/Hero.tsx
@@ -17,7 +17,7 @@ export default function Hero({}: Props) {
    
         <img className="relative rounded-full h-32 w-32 mx-auto object-cover" src="https://i.ibb.co/N7qq026/jenny0.jpg" />
         <h2 className="text-sm uppercase text-gray-500 pb-2 tracking-[15px]">Software Engineer</h2>
-        <h1 className="text-5xl lg:tesxt-6xl font-semibold scroll-px-10">
+        <h1 className="text-4xl lg:tesxt-6xl font-semibold scroll-px-10">
             <span className="mr-3">{text}</span>
             <Cursor cursorColor="#F7AB0A" />
         </h1>
