c4e2d695533562c077ead067451a878caf67e89b
Jinyeong <jinyeongpark1205@gamil.com>
2022-11-30 11:00:56 -0800

    here button components and created menu button

--- portfolio-nextjs/components/Hero.tsx
+++ portfolio-nextjs/components/Hero.tsx
@@ -1,6 +1,9 @@
+import Link from 'next/link';
 import React from 'react';
 import { Cursor, useTypewriter } from 'react-simple-typewriter';
 import BackgroundCircles from "../components/BackgroundCircles";
+
+
 
 type Props = {}
 
@@ -16,11 +19,28 @@ export default function Hero({}: Props) {
         <BackgroundCircles />
    
         <img className="relative rounded-full h-32 w-32 mx-auto object-cover" src="https://i.ibb.co/N7qq026/jenny0.jpg" />
-        <h2 className="text-sm uppercase text-gray-500 pb-2 tracking-[15px]">Software Engineer</h2>
-        <h1 className="text-4xl lg:tesxt-6xl font-semibold scroll-px-10">
-            <span className="mr-3">{text}</span>
-            <Cursor cursorColor="#F7AB0A" />
-        </h1>
+        <div className="z-20">
+          <h2 className="text-sm uppercase text-gray-500 pb-2 tracking-[15px]">Software Engineer</h2>
+          <h1 className="text-5xl lg:tesxt-6xl font-semibold scroll-px-10">
+              <span className="mr-3">{text}</span>
+              <Cursor cursorColor="#F7AB0A" />
+          </h1>
+          <div className="pt-5">
+            <Link href="#about">
+              <button className="heroButton">About</button>
+            </Link>
+            <Link href="#experience">
+            <button className="heroButton">Experience</button>
+            </Link>
+            <Link href="#skills">
+            <button className="heroButton">Skills</button>
+            </Link>
+            <Link href="#projects">
+            <button className="heroButton">Projects</button>
+            </Link>
+          </div>
+        </div>
+
 
     </div>
   )
--- portfolio-nextjs/pages/index.tsx
+++ portfolio-nextjs/pages/index.tsx
@@ -1,6 +1,5 @@
 import type { NextPage } from "next";
 import Head from 'next/head'
-import styles from '../styles/Home.module.css'
 import Header from "../components/Header";
 import Hero from "../components/Hero";
 
