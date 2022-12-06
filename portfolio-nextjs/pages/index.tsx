d6d870009786191ce8d0ddb2280806377cbccd84
Jinyeong <jinyeongpark1205@gamil.com>
2022-12-05 16:21:17 -0800

    [Projects] section

--- portfolio-nextjs/pages/index.tsx
+++ portfolio-nextjs/pages/index.tsx
@@ -4,6 +4,7 @@ import About from "../components/About";
 import Experience from "../components/Experience";
 import Header from "../components/Header";
 import Hero from "../components/Hero";
+import Projects from "../components/Projects";
 import Skills from "../components/Skills";
 
 
@@ -38,6 +39,9 @@ const Home: NextPage = () => {
       </section>
 
       {/* - Projects  */}
+      <section id="projects" className="snap-center">
+        <Projects />
+      </section>
       
       {/* - Contact Us */}
     </div>
